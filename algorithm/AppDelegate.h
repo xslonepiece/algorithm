b60ed806e782596f7254eeca7bf5c97627dfa0c4
sili_xia <sili_xia@kingdee.com>
2018-11-02 16:28:06 +0800

    初始化工程

---
+++ algorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  algorithm
+//
+//  Created by sili_xia on 2018/11/2.
+//  Copyright © 2018 sili_xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ algorithm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  algorithm
+//
+//  Created by sili_xia on 2018/11/2.
+//  Copyright © 2018 sili_xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
